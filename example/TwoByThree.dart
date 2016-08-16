0637558c0f6f94e449c0d14f7f4059c45e0ff551
CptCrunch117 <hanbrolo117@gmail.com>
2016-08-15 18:17:46 -0700

    added a bit of sugar to the TwoByThree example class.

--- example/TwoByThree.dart
+++ example/TwoByThree.dart
@@ -1,18 +1,31 @@
 import 'dart:io';
 import '../lib/NMatrix.dart';
 
-main(){
-  NMatrix<int> twoByThree = new NMatrix<int>(2,[2,3],1);
+main() {
+  NMatrix<int> twoByThree = new NMatrix<int>(2, [2, 3], 1);
 
   stdout.writeln();
-  stdout.writeln("A Dynamic Dimensional Integer Matrix of the form ${twoByThree.getDimensionSizes()[0]}x${twoByThree.getDimensionSizes()[1]}:");
-  for(int i=0; i < twoByThree.getDimensionSizes()[0]; i++){
-    for(int j = 0; j < twoByThree.getDimensionSizes()[1]; j++){
-        stdout.write(twoByThree.getAt([i,j]).toString()+"\t");
+  stdout.writeln(
+      "A Dynamic Dimensional Integer Matrix of the form ${twoByThree.getDimensionSizes()[0]}x${twoByThree.getDimensionSizes()[1]}:");
+  for (int i = 0; i < twoByThree.getDimensionSizes()[0]; i++) {
+    for (int j = 0; j < twoByThree.getDimensionSizes()[1]; j++) {
+      stdout.write(twoByThree.getAt([i, j]).toString() + "\t");
     }
     stdout.writeln();
   }
 
+  NMatrix<String> twoByThreeS = new NMatrix<String>(2, [3, 3], "Hey!");
 
+  stdout.writeln();
+  stdout.writeln(
+      "A Dynamic Dimensional String Matrix of the form ${twoByThreeS.getDimensionSizes()[0]}x${twoByThreeS.getDimensionSizes()[1]}:");
+  for (int i = 0; i < twoByThreeS.getDimensionSizes()[0]; i++) {
+    for (int j = 0; j < twoByThreeS.getDimensionSizes()[1]; j++) {
+      stdout.write(twoByThreeS.getAt([i, j]).toString() + "\t");
+    }
+    stdout.writeln();
+  }
 
+  stdout.writeln("In the source code the only change I had to make was the generic type and dimension sizes in the constructor.\n"
+      "The remaining code stayed unchanged. This makes it very easy to create very different types of matrices with little work!");
 }
