42837c4746894ddbedc27b1bc626293452a224aa
CptCrunch117 <hanbrolo117@gmail.com>
2016-08-23 17:46:41 -0700

    Getting better at asynchronous programming with Dart. Added a Java Test program to compare against the Dart Benchmark to get a relative perspective of this data structure vs other languages. I want to soon implement the C++ tests. Also cleaned up the logging and output of the benchmark test.

--- benchmark/BenchMark01.dart
+++ benchmark/BenchMark01.dart
@@ -1,15 +1,68 @@
 import 'dart:io';
+import 'dart:async';
 import 'dart:core';
 import '../lib/dart_constructs.dart';
-void main(){
+
+
+Future main()async {
+  Stopwatch benchmark = new Stopwatch();
+
+  stdout.writeln("\nBuild Log:");
+  stdout.writeln("-------------------------");
+  stdout.writeln("\nExecuting Java Code for primitive datatype build...");
+  benchmark.start();
+  ProcessResult t1 = await javaOneTest();
+  stdout.writeln("Java build complete.");
+  stdout.writeln("Executing Java Code for object datatype build...");
+  ProcessResult t2 = await javaTwoTest();
+  stdout.writeln("Java build complete.");
+  stdout.writeln("Dart is running first object datatype build...");
+  Stopwatch fwatch1 = await dartOneTest();
+  stdout.writeln("Dart build complete.");
+  stdout.writeln("Dart is running second object datatype build...");
+  Stopwatch fwatch2 = await dartOneTest();
+  benchmark.stop();
+  stdout.writeln("Dart build complete.");
+  stdout.writeln("-------------------------");
+
+  stdout.writeln("\nBenchMark Results:");
+  stdout.writeln("-------------------------\n");
+
+  stdout.writeln("Dart_NMatrix build result: ${fwatch1.elapsedMilliseconds/1000} seconds to build 400000000 dataPoints.");
+  stdout.writeln("- VS -");
+  stdout.writeln("Java build-Primitive result: ${t1.stdout}\n");
+
+  stdout.writeln("Note: This is an object data type matrix versus a primitive datatype matrix.\n"
+      "The second test is an object datatype vs object data type.\n");
+
+
+  stdout.writeln("Dart_NMatrix build result: ${fwatch2.elapsedMilliseconds/1000} seconds to build 400000000 dataPoints.");
+  stdout.writeln("- VS -");
+  stdout.writeln("Java build-Object result: ${t2.stdout}");
+  stdout.writeln("\n-------------------------\n");
+
+  stdout.writeln("\nBenchmark Test took: ${benchmark.elapsedMilliseconds/1000} seconds");
+
+
+}
+
+Stopwatch createMatrixTest(){
   Stopwatch watch = new Stopwatch();
   watch.start();
   NMatrix<int> nm3d = new NMatrix.dimensional([20000,20000], 50);
   watch.stop();
-  stdout.writeln();
-  stdout.writeln("Time Elapsed in seconds to build ${nm3d.dimSizes[0]*nm3d.dimSizes[1]} datapoints: ${watch.elapsedMilliseconds/1000}");
-  //stdout.writeln("To build: \n ${nm3d.toString()}");
+  return watch;
+}
 
+Future<Stopwatch> dartOneTest() async{
+  Future<Stopwatch> f = new Future(() => createMatrixTest());
+  return f;
+}
 
+Future<ProcessResult> javaOneTest() async{
+  return Process.run('java',['-cp','benchmark/java','javaTest01','one']);
+}
 
+Future<ProcessResult> javaTwoTest() async{
+  return Process.run('java',['-cp','benchmark/java','javaTest01','two']);
 }
